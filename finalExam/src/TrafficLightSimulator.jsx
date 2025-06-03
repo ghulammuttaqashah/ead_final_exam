6265b0962f5c9c01d24e95a3e39c9db398807784
Ghulam Muttaqa Shah <ghulammuttaqashah@gmail.com>
2025-06-03 12:04:59 +0500

    done the initial code not completed

--- finalExam/src/TrafficLightSimulator.jsx
+++ finalExam/src/TrafficLightSimulator.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react"
+import { useEffect } from "react"
 function TrafficLightSimulator()
 {
+    const [red,setRed]=useState('red')
+    const [yellow,setYellow]=useState('yellow')
+    const [green,setGreen]=useState('green')
+    
+
+
+    useEffect(()=>{
+        setInterval(()=>{
+            let value=document.getElementById('red')
+            value.style='background-color:grey'
+        },3000)
+    },[])
     return(<>
         <p>Taffic Light Simulator</p>
     <div id="main">
