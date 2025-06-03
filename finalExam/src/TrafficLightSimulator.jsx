4f25e06d4d0ad1e6b079f03a1596d23383cbde52
Ghulam Muttaqa Shah <ghulammuttaqashah@gmail.com>
2025-06-03 11:35:53 +0500

    converted the html into react component Taffic Light Simulator used global css index.css for styling to tailwind installing issues

--- finalExam/src/TrafficLightSimulator.jsx
+++ finalExam/src/TrafficLightSimulator.jsx
@@ -1,7 +1,12 @@
 function TrafficLightSimulator()
 {
     return(<>
-        
+        <p>Taffic Light Simulator</p>
+    <div id="main">
+            <div id="red"></div>
+            <div id='yellow'></div>
+            <div id="green"></div>
+        </div>
     </>)
 }
 export default TrafficLightSimulator
